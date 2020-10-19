a7d263fa5dbc8213e5a56ec1caf7108213584c1a
gd7 <gd7@DESKTOP-MBB9HSL>
2020-10-19 16:35:18 +0900

    프로젝트 생성, 패키지 구조 생성, 필터(Encoding,Login)생성

---
+++ sakila/src/sakila/filter/EncodingFilter.java
@@ -0,0 +1,37 @@
+package sakila.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+/**
+ * Servlet Filter implementation class EncodingFilter
+ */
+@WebFilter("/*")
+public class EncodingFilter implements Filter {
+
+
+    public EncodingFilter() {
+        // TODO Auto-generated constructor stub
+    }
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+		System.out.println("인코딩필터 실행 : request utf-8 인코딩");
+		request.setCharacterEncoding("utf-8");
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+		// TODO Auto-generated method stub
+	}
+
+}
